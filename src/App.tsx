2a1dd51e7bc77f17b6f5c630cc13c591af352135
Puskar Adhikari <puskar.ad@gmail.com>
2022-12-27 22:05:38 +0545

    example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
 
-import { ScrollingCarousel } from 'components'
+import { ScrollingCarousel, Carousel } from 'components'
 
 function App() {
+  const uuid = crypto.randomUUID()
   const items = [
     'Slide 1',
     'Slide 2',
@@ -20,13 +21,37 @@ function App() {
   return (
     <div className='app'>
       <div className='app-container'>
+        <p>Swipeable Carousel</p>
+        <Carousel
+          show={3.5}
+          slide={3}
+          swiping={true}
+          className='swipe-slider'
+          hideArrows={true}
+        >
+          <div className='swipe-item'>Slide 1</div>
+          <div className='swipe-item'>Slide 2</div>
+          <div className='swipe-item'>Slide 3</div>
+          <div className='swipe-item'>Slide 4</div>
+          <div className='swipe-item'>Slide 5</div>
+          <div className='swipe-item'>Slide 6</div>
+          <div className='swipe-item'>Slide 7</div>
+          <div className='swipe-item'>Slide 8</div>
+          <div className='swipe-item'>Slide 9</div>
+          <div className='swipe-item'>Slide 10</div>
+          <div className='swipe-item'>Slide 11</div>
+          <div className='swipe-item'>Slide 12</div>
+        </Carousel>
+      </div>
+      <div className='app-container'>
+        <p>Scrolling Carousel</p>
         <ScrollingCarousel
           className='scroll-slider'
           rightIcon={<></>}
           leftIcon={<></>}
         >
           {items?.map((item) => (
-            <div className='item' key='item'>
+            <div className='item' key={item + uuid}>
               {item}
             </div>
           ))}
